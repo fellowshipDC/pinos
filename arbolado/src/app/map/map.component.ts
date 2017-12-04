a82ae186e8f0dc09b81daafb6ce6c149c3fe3b09
unknown <mirandas.angelica@gmail.com>
2017-12-03 21:39:40 -0600

    leaflet installed and working

--- arbolado/src/app/map/map.component.ts
+++ arbolado/src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -9,12 +10,26 @@ import { Http, Response } from '@angular/http';
 export class MapComponent implements OnInit {
 
   data: any;
+  mymap: any;
 
   constructor(private http: Http) { }
 
   ngOnInit() {
-    this.getData();
+    //this.getData();
+    this.mymap = L.map('mapid').setView([20.831364, -100.478561], 5);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1Ijoic2F0aXJhbWEiLCJhIjoiY2phcmhpZWxjNGppaDJ3cGwyYmp0NGVtZyJ9.1E3t6hV_CYLzQ_0Ba1IFmQ'
+    }).addTo(this.mymap);
+    //{
+      //center: [20.831364, -100.478561],
+      //zoom: 1
+    //});
   }
+
+  
 
   getData() {
     this.http.get('http://localhost:3000/forest').subscribe((res: Response) => this.data = res.json())
