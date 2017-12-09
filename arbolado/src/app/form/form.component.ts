803a434b0b3b6f9db05f16cab4828c1411f6a69d
unknown <mirandas.angelica@gmail.com>
2017-12-09 15:08:28 -0600

    working on form button

--- arbolado/src/app/form/form.component.ts
+++ arbolado/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  formSent: any;
+  url= 'http://localhost:3000/forest/form';
+  data= {opinion: 'as'};
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
+  }
+
+  submit(){
+    console.log('submit button works');
+    var headrs = new Headers();
+    headrs.append('Content-Type', 'application/json');
+    headrs.append('Accept', 'application/json');
+
+    this.http.post(this.url, this.data, {headers: headrs})
+    .subscribe((res: Response)=> {this.formSent = res.json(); console.log('done');})
   }
 
 }
