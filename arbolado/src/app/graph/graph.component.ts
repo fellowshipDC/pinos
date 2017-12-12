8ac70f5aeb335a059027fc98b813f27769139ebe
unknown <mirandas.angelica@gmail.com>
2017-12-12 12:22:56 -0600

    graph with data from server + both y-axis

--- arbolado/src/app/graph/graph.component.ts
+++ arbolado/src/app/graph/graph.component.ts
@@ -10,264 +10,9 @@ import * as d3 from 'd3';
 export class GraphComponent implements OnInit {
 
   info: any;
-  data = [
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940251,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2001,
-      "value": 36.76790697,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940257,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2002,
-      "value": 36.34774944,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940263,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2003,
-      "value": 33.60308211,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940269,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2004,
-      "value": 37.96013524,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940275,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2005,
-      "value": 46.85256588,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940281,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2006,
-      "value": 39.76199641,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940287,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2007,
-      "value": 50.70297395,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940293,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2008,
-      "value": 42.58655714,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940299,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2009,
-      "value": 62.88570811,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940305,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2010,
-      "value": 39.84923655,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940311,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2011,
-      "value": 43.49183047,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940317,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2012,
-      "value": 41.02147757,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940323,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2013,
-      "value": 50.93828439,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940329,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2014,
-      "value": 39.71296392,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940335,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2015,
-      "value": 45.97435274,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    },
-    {
-      "indicator_id": 14,
-      "cartodb_id": 940341,
-      "iso": "MEX",
-      "sub_nat_id": "",
-      "boundary": "admin",
-      "boundary_id": 1,
-      "thresh": 10,
-      "country": "Mexico",
-      "year": 2016,
-      "value": 65.00171987,
-      "province": "",
-      "boundary_name": "administrative boundary",
-      "indicator_name": "AGC loss WH in MtC02",
-      "units": "MtC02"
-    }
-  ]
+  dataCo2: Array<any>;
+  dataHa: Array<any>;
+
   constructor(private http: HttpClient) { }
 
   d3graph(){
@@ -277,6 +22,7 @@ export class GraphComponent implements OnInit {
     var h = 500 - (margin.top + margin.bottom);
     var parse = d3.timeParse("%b %Y");
 
+    //graph svg
     var graph = d3.select('#graphid')
       .append('svg')
       .attr('width', w + (margin.left + margin.right))
@@ -285,13 +31,18 @@ export class GraphComponent implements OnInit {
       .append('g')
         .attr('transform', 'translate('+ margin.left + ',' + margin.top + ')');
 
-    var data = this.data.sort((a,b) => d3.ascending(a.year, b.year));
-    var xScale = d3.scaleLinear().domain([0, 15]).range([0, w])
-    var yScale = d3.scaleLinear().domain([70, 0]).range([0, h])
+    //sort
+    var co2 = this.dataCo2.sort((a,b) => d3.ascending(a.year, b.year));
+    var ha = this.dataHa.sort((a,b) => d3.ascending(a.year, b.year));
+
+    //scales
+    var xScale = d3.scaleLinear().domain([0, 15]).range([0, w]);
+    var yScaleCo2 = d3.scaleLinear().domain([70, 0]).range([0, h]);
+    var yScaleHa = d3.scaleLinear().domain([300, 140]).range([0, h]);
 
     var line = d3.line()
       .x(function(d, i) {console.log(i); return xScale(i);})
-      .y(function(d) { console.log(d.value); return yScale(d.value);});
+      .y(function(d) { console.log(d.value); return yScaleCo2(d.value);});
 
     graph.append("g")
       .attr("class", "x axis")
@@ -300,19 +51,24 @@ export class GraphComponent implements OnInit {
 
     graph.append("g")
       .attr("class", "y axis")
-      .call(d3.axisLeft(yScale));
+      .call(d3.axisLeft(yScaleCo2));
+
+    graph.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate("+ w + ", 0)")
+      .call(d3.axisRight(yScaleHa));
 
     var path = graph.append('path')
-      .datum(data)
-      .attr('class', 'line')
+      .datum(co2)
+      .attr('class', 'lineCo2')
       .attr('d', line);
 
     graph.selectAll('.dot')
-      .data(data)
+      .data(co2)
       .enter().append('circle')
-        .attr('class', 'dot')
+        .attr('class', 'dotCo2')
         .attr('cx', function(d, i){ return xScale(i) })
-        .attr('cy', function(d){ return yScale(d.value) })
+        .attr('cy', function(d){ return yScaleCo2(d.value) })
         .attr('r', 5);
 
     var curtain = graph.append('rect')
@@ -336,7 +92,6 @@ export class GraphComponent implements OnInit {
   }
 
   ngOnInit() {
-    //this.d3graph();
     this.getData();
   }
 
@@ -344,15 +99,28 @@ export class GraphComponent implements OnInit {
     this.http.get('http://localhost:3000/forest/')
       .subscribe(
         res => {
-          console.log('requested');
-          this.info = res.data.filter((obj)=>{
-            if (obj.indicator_id == 14 && obj.sub_nat_id == undefined && obj.boundary_id ==1 && obj.thresh == 10){
-              return true;
+          this.info = res;
+          if (this.info !== undefined){
+            this.dataCo2 = this.info.data.filter((obj)=>{
+              if (obj.indicator_id == 14 && obj.sub_nat_id == undefined && obj.boundary_id ==1 && obj.thresh == 10){
+                return true;
+              }
+              else{
+                return false;
+              }
+            });
+            this.dataHa = this.info.data.filter((obj)=>{
+              if (obj.indicator_id == 1 && obj.sub_nat_id == undefined && obj.boundary_id ==1 && obj.thresh == 10){
+                return true;
+              }
+              else{
+                return false;
+              }
+            });
+            if(this.dataCo2.length > 0 && this.dataHa.length > 0){
+              this.d3graph();
             }
-            else{
-              return false;
-            }
-          });
+          }
         },
         err => {
           console.log('error');
